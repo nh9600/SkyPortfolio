a5778f352d099899f503bb488f2619a9f320b74c
nh9600 <nh9600@daum.net>
2022-12-04 02:24:59 +0900

    feat: 기본 setting 및 메인 화면 설정

---
+++ src/main/java/com/sky/portfolio/SkyPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.sky.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/portfolio/SkyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
